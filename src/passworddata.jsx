c05dba9b5f6cc6ef956bd1748cc44110059f7ba3
Azimbashar <140090743+Azimbashar@users.noreply.github.com>
2025-09-13 16:05:09 +0530

    Update passworddata.jsx

--- src/passworddata.jsx
+++ src/passworddata.jsx
@@ -1,4 +1,4 @@
 export  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export  let lowercase = "abcdefghijklmnopqrstuvwxyz";
-export  let symbolscase = "!@#$%^&*()_+{}<>|\/?-,.";
+export  let symbolscase = "!@#$%^&*()_+{}<>|?-,.";
 export  let numbercase = "1234567890";
